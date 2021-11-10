6c84f64a8dd77f66939466e0db4ec550a02da434
Dark Phoenix <83725513+HarshRaj154@users.noreply.github.com>
2021-11-10 12:29:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ cat_key = config.get('cat_key')
 weather_key = config.get('weather_key')
 intents = disnake.Intents().all()
 bitly_key = config.get('bitly_key')
-Yui = commands.Bot(command_prefix='san', strip_after_prefix = True, intents = intents, sync_commands_debug=True)
+Yui = commands.Bot(command_prefix=commands.when_mentioned_or('Luv', 'Eru', 'eru', 'luv', 'love', 'Love'), strip_after_prefix = True, intents = intents, sync_commands_debug=True)
 together_control = DisnakeTogether(Yui)
 Yui.remove_command('help')
 
