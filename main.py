cdd8637dc30f3d66d9a54a2382db49aa1efc0302
Phoenix <phoenex659@gmail.com>
2021-11-30 15:12:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1722,6 +1722,6 @@ async def loadall(ctx):
                 await ctx.send(f"Loaded `{filename[:-3]}` successfully!")
 
 Yui.load_extension("cogs.fun")
-Yui.load_extension("cogs.music")
+#Yui.load_extension("cogs.music")
 Yui.load_extension("cogs.EvalCommand")
 Yui.run(token)
