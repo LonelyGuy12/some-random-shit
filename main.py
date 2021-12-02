6e8093ea81963755cd235bddb7891d3d3eaaf4d5
Dark Phoenix <83725513+HarshRaj154@users.noreply.github.com>
2021-12-02 15:52:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1218,7 +1218,8 @@ async def help(ctx, *, cmd = None):
                 embedVar = disnake.Embed(title=cmd, description=desc, color=bot_embed_color)
                 await ctx.reply(embed=embedVar)
         except:
-            desc = help.get(cmd)
+            embedVar = disnake.Embed(title="Command not found", description="Please check the command name", color=bot_embed_color)
+            await ctx.reply(embed=embedVar)
             
 
 @bot.user_command(name="Avatar") # optional
