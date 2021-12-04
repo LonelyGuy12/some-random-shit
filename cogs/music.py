3dd9be53297020329deba2902f5ef81407919e91
Phoenix <phoenex659@gmail.com>
2021-12-04 12:08:39 +0530

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -317,7 +317,7 @@ class Music(commands.Cog, wavelink.WavelinkMixin):
                           'rest_uri': 'https://lava.link:80',
                           'password': 'anything as password',
                           'identifier': 'MAIN',
-                          'region': 'us_central',
+                          'region': 'germany',
                           'secure': 'true'
                           }}
 
