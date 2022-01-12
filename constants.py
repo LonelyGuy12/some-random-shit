3497ea6b8c2fccf40d255efbf4a92c713c72bb8e
Lonely Guy <phoenex659@gmail.com>
2022-01-12 22:22:55 +0530

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,71 @@
+"""
+A file where most the constants are stored
+"""
+
+
+class Replies:
+    """
+    A Class that stores Replies
+    ---
+    List -> postive_replies,negative_replies
+    Methods -> None
+    """
+
+    welcome_back_replies = [
+        "Great to see you again!",
+        "Hey, you've been gone for some time, we've missed you.",
+        "Some time has passed.",
+        "Oh look, it's you again!",
+        "No matter how far we may wander from the Lord’s perfect will for our lives, we are always welcome back!",
+    ]
+    positive_replies = [
+        "Yep.",
+        "Absolutely!",
+        "Can do!",
+        "Affirmative!",
+        "Yeah okay.",
+        "Sure.",
+        "Sure thing!",
+        "You're the boss!",
+        "Okay.",
+        "No problem.",
+        "I got you.",
+        "Alright.",
+        "You got it!",
+        "ROGER THAT",
+        "Of course!",
+        "Aye aye, cap'n!",
+        "I'll allow it.",
+    ]
+
+    error_replies = [
+        "Please don't do that.",
+        "You have to stop.",
+        "Do you mind?",
+        "In the future, don't do that.",
+        "That was a mistake.",
+        "You blew it.",
+        "You're bad at computers.",
+        "Are you trying to kill me?",
+        "Noooooo!!",
+        "I can't believe you've done this",
+    ]
+
+
+class Colors:
+    """
+    A class to store all the used Colors.
+    HexCode -> Colors
+    Methods -> None
+    """
+
+    blue = 0x0000FF
+    red = 0x992D22
+    green = 0x1F8B4C
+    yellow = 0xFFFF00
+    pink = 0xFFC0CB
+    gray = 0x808080
+    black = 0x000000
+    purple = 0xA020F0
+    violet = 0x8F00FF
+    lavender = 0xE6E6FA
